a8d61358dca46dcd306f15a980d4a397b1fd3f64
Thanh Tung <thanhtung05@gmail.com>
2026-07-25 13:51:24 +0700

    cap nhat lai phien ban

--- src/test/java/com/travelplanner/TravelPlannerApplicationTests.java
+++ src/test/java/com/travelplanner/TravelPlannerApplicationTests.java
@@ -10,8 +10,6 @@ class TravelPlannerApplicationTests {
 
     @Test
     void contextLoads() {
-        // Test nay chi de dam bao Spring context (tat ca @Bean, @Configuration)
-        // khoi tao thanh cong, khong co loi wiring. Day la test "smoke test"
-        // nen co ngay tu dau du an.
+        
     }
 }
